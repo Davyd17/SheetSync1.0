e91578818b2f9fbbc77a438125b155d0e073d766
David <davidcq55@gmail.com>
2025-04-10 14:39:12 -0500

    feat: add Sheet and SpreadSheet repositories with JPA implemented

---
+++ src/main/java/com/demo/sheetsync/repository/SpreadSheetRepository.java
@@ -0,0 +1,10 @@
+package com.demo.sheetsync.repository;
+
+import com.demo.sheetsync.model.entity.SpreadSheet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SpreadSheetRepository extends JpaRepository<SpreadSheet, Integer> {
+
+}
