fa9fe03d788ee50b28367e2d822038654d9269a3
David <davidcq55@gmail.com>
2025-05-12 14:56:29 -0500

    fix: correct JpaRepository generic type in SheetRepository interface

--- src/main/java/com/demo/sheetsync/repository/SheetRepository.java
+++ src/main/java/com/demo/sheetsync/repository/SheetRepository.java
@@ -1,10 +1,11 @@
 package com.demo.sheetsync.repository;
 
+import com.demo.sheetsync.model.entity.Sheet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SheetRepository extends JpaRepository<SheetRepository, Integer> {
+public interface SheetRepository extends JpaRepository<Sheet, Integer> {
 
 
 }
