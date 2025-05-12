ea24f3d204fcd7698343580d4ef998f0955da779
David <davidcq55@gmail.com>
2025-05-12 14:20:04 -0500

    refactor: inject Sheets service into SheetService via constructor
        - Removed manual instantiation of Sheets service using GoogleAuth
        - Injected pre-configured Sheets bean from GoogleSheetsConfig
        - Added dependencies: SpreadSheetRepository, SpreadSheetInfoRequest, and SheetDataResponse
        - Prepared getSheetData method to consume incoming request and range

--- src/main/java/com/demo/sheetsync/service/SheetService.java
+++ src/main/java/com/demo/sheetsync/service/SheetService.java
@@ -1,10 +1,12 @@
 package com.demo.sheetsync.service;
 
-import com.demo.sheetsync.auth.GoogleAuth;
+import com.demo.sheetsync.config.googlesheets.GoogleAuth;
+import com.demo.sheetsync.model.entity.dto.request.SpreadSheetInfoRequest;
+import com.demo.sheetsync.model.entity.dto.response.SheetDataResponse;
 import com.demo.sheetsync.repository.SheetRepository;
+import com.demo.sheetsync.repository.SpreadSheetRepository;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.time.temporal.ValueRange;
 
 @Service
 @RequiredArgsConstructor
@@ -19,18 +22,16 @@ public class SheetService {
 
     private final SheetRepository sheetRepository;
 
-    private Sheets provideSheetService() throws GeneralSecurityException, IOException {
+    private final SpreadSheetRepository spreadSheetRepository;
 
-        NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+    private final Sheets sheets;
 
-        return new Sheets.Builder(
-                httpTransport,
-                GsonFactory.getDefaultInstance(),
-                GoogleAuth.getCredentials(httpTransport)
-        ).build();
+    public SheetDataResponse getSheetData(SpreadSheetInfoRequest spreadSheetRequest, String range){
+
+        return null;
     }
 
-    
+
 
 
 }
