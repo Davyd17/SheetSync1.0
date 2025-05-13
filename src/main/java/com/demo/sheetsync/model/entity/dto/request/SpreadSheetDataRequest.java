1e95036c42484b342e6b5d32568fcdbc33eba173
David <davidcq55@gmail.com>
2025-05-12 21:28:28 -0500

    feat: add request and response DTOs for spreadsheet data
        - Added `SpreadSheetDataRequest` to encapsulate the spreadsheet ID in incoming requests.
        - Added `SpreadSheetDataResponse` to return spreadsheet metadata including ID, title, and associated sheets.
        - These DTOs support cleaner separation of concerns between internal models and API contracts.

---
+++ src/main/java/com/demo/sheetsync/model/entity/dto/request/SpreadSheetDataRequest.java
@@ -0,0 +1,15 @@
+package com.demo.sheetsync.model.entity.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SpreadSheetDataRequest {
+
+    private String spreadSheetId;
+}
