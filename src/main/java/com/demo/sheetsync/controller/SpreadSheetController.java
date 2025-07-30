1df0b99d061d336d915474a283bc449e42883e1f
David <davidcq55@gmail.com>
2025-07-29 22:18:06 -0500

    refactor(controller): change GET to POST for saving spreadsheet and add retrieval endpoint

--- src/main/java/com/demo/sheetsync/controller/SpreadSheetController.java
+++ src/main/java/com/demo/sheetsync/controller/SpreadSheetController.java
@@ -13,12 +13,20 @@ public class SpreadSheetController {
 
     private final SpreadSheetService service;
 
-    @GetMapping("/{spreadSheetId}")
+    @PostMapping("persist/{spreadSheetId}")
     public ResponseEntity<SpreadSheetResponse> saveFromGoogleSheet(@PathVariable String spreadSheetId) {
 
         return ResponseEntity.ok(
                 service.saveSpreadSheet(spreadSheetId)
         );
 
+    }
+
+    @GetMapping("get/{spreadSheetId}")
+    public ResponseEntity<SpreadSheetResponse> getSpreadSheet(@PathVariable String spreadSheetId){
+
+        return ResponseEntity.ok(
+                service.getSpreadSheet(spreadSheetId)
+        );
     }
 }
