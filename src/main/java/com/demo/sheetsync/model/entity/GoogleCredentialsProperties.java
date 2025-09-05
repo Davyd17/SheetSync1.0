11338d692449edc4b9ce956131307e98e0fe5d73
David <davidcq55@gmail.com>
2025-09-05 10:19:24 -0500

    refactor(credentials): remove redundant Lombok annotations from record
        - Dropped @RequiredArgsConstructor and @Getter
        - Simplified GoogleCredentialsProperties since records generate constructor and accessors automatically

--- src/main/java/com/demo/sheetsync/model/entity/GoogleCredentialsProperties.java
+++ src/main/java/com/demo/sheetsync/model/entity/GoogleCredentialsProperties.java
@@ -3,13 +3,9 @@ package com.demo.sheetsync.model.entity;
 
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import java.util.Collection;
 
-@RequiredArgsConstructor
-@Getter
 public record GoogleCredentialsProperties(
         
         String TOKENS_DIRECTORY_PATH, Collection<String> SCOPES,
